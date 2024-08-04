1a3fc2ca586fd50b8cc665514d3a3226a2e5e8ce
RedZodiacX <locurax41@hotmail.com>
2024-08-04 14:41:14 -0500

    react

---
+++ src/Components/funtions.js
@@ -0,0 +1,17 @@
+var nombre = 'Andres'
+const apellido = 'Umenza'
+let edad = 20
+const mycar = new Object();
+mycar.make = 'Ford';
+mycar.model = 'Mustang';
+mycar.year = 1969;
+const myarray = [1, 2, 3, 4];
+myarray.push(5)
+
+
+
+console.log(myarray);
+console.log(`Hola mi nombre es ${nombre} y mi apellido es ${apellido} tengo ${edad} años `);
+console.log(mycar);
+
+export default Functions;
